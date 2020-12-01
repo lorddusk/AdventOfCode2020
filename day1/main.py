c55d54044a6a581f09528edefa781b603a47f6b3
LordDusk <lang2619@gmail.com>
2020-12-01 15:03:34 +0100

    feat(day1): Cleanup on day 1

--- day1/main.py
+++ day1/main.py
@@ -1,42 +1,14 @@
-def examplepart1(input):
-    print("EXAMPLE PART 1")
-    for i in range(0, len(input)):
-        for j in range(0, len(input)):
-            output = input[i] + input[j]
-            if output == 2020:
-                output = input[i] * input[j]
-                print(f"{input[i]} x {input[j]} = {output}")
-                print("------------------------------------------------------")
-                return
-
-
 def part1(input):
-    print("PART 1")
     for i in range(0, len(input)):
         for j in range(0, len(input)):
             output = input[i] + input[j]
             if output == 2020:
                 output = input[i] * input[j]
                 print(f"{input[i]} x {input[j]} = {output}")
-                print("------------------------------------------------------")
                 return
-
-
-def examplepart2(input):
-    print("EXAMPLE PART 2")
-    for i in range(0, len(input)):
-        for j in range(0, len(input)):
-            for k in range(0, len(input)):
-                output = input[i] + input[j] + input[k]
-                if output == 2020:
-                    output = input[i] * input[j] * input[k]
-                    print(f"{input[i]} x {input[j]} x {input[k]} = {output}")
-                    print("------------------------------------------------------")
-                    return
 
 
 def part2(input):
-    print("PART 2")
     for i in range(0, len(input)):
         for j in range(0, len(input)):
             for k in range(0, len(input)):
@@ -44,7 +16,6 @@ def part2(input):
                 if output == 2020:
                     output = input[i] * input[j] * input[k]
                     print(f"{input[i]} x {input[j]} x {input[k]} = {output}")
-                    print("------------------------------------------------------")
                     return
 
 
@@ -55,7 +26,16 @@ if __name__ == '__main__':
     f = open('../inputs/day1.txt', 'r')
     for x in f:
         input.append(int(x))
-    examplepart1(exampleInput)
+
+    print("EXAMPLE PART 1")
+    part1(exampleInput)
+    print("------------------------------------------------------")
+    print("PART 1")
     part1(input)
-    examplepart2(exampleInput)
+    print("------------------------------------------------------")
+    print("EXAMPLE PART 2")
+    part2(exampleInput)
+    print("------------------------------------------------------")
+    print("PART 2")
     part2(input)
+    print("------------------------------------------------------")
